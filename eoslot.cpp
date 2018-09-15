5074aef64b57c28b794a514b963397115b303149
apple <apple@appledeMacBook-Pro.local>
2018-09-15 20:47:23 +0800

    fix bug

--- eoslot.cpp
+++ eoslot.cpp
@@ -430,22 +430,5 @@ namespace eosio
   }
 }
 
-#define EOSIO_ABI_EX( TYPE, MEMBERS ) \
-extern "C" { \
-   void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
-      auto self = receiver; \
-      if( action == N(onerror)) { \
-         \
-         eosio_assert(code == N(eosio), "onerror action's are only valid from the \"eosio\" system account"); \
-      } \
-      if( code == self || code == N(eosio.token) || action == N(onerror) ) { \
-         TYPE thiscontract( self ); \
-         switch( action ) { \
-            EOSIO_API( TYPE, MEMBERS ) \
-         } \
-         \
-      } \
-   } \
-}
 
-EOSIO_ABI_EX(eosio::eoslot, (create)(issue)(transfer)(refund)(init))
+EOSIO_ABI(eosio::eoslot, (create)(issue)(transfer)(refund)(init))
